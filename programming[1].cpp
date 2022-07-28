1edf60b25e7a20beda9750c9a7f5091b44e164bc
Liliose-wimax <110086304+Liliose-wimax@users.noreply.github.com>
2022-07-28 21:47:46 +1400

    Add files via upload

---
+++ programming[1].cpp
@@ -0,0 +1,36 @@
+//program that allows a user to create an e-wallet account
+#include<iostream>
+using namespace std;
+int main(){
+	//int createaccount;
+	string username;
+	string password;
+	int balance=1000;
+	int money;
+	int amount;
+	int account;
+	cout<<"please create a username"<<endl;
+	cin>>username;
+		cout<<"create a password"<<endl;
+		cin>>password;
+		cout<<"account created!"<<endl;
+		cout<<"enter the account you want to transfer the money to"<<endl;
+		if (amount=1 $$ amount<10000){
+                                   cout<<"No transaction fees";
+else if(amount=10000 $$ amount<100000)
+cout<<"transfer fees is 200"<<endl;
+else if(amount>=100000)
+cout<<"transaction fees is 1000"<<endl;
+else
+cout<<"you are not allowed to transfer";
+}
+cout<<"enter your password"<<endl;
+cin>>password;
+if(password==abcdef){
+cout<<"the transaction is done successfully!!!"<<endl;
+else
+cout<<"your password is incorrect, please try again "
+}
+return 0;
+}
+                                                                                                           
